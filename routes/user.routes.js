b57f676fc8d36c3c921b7b83653c619e119df471
Mayank-MP05 <mayank5pande@gmail.com>
2022-05-21 01:32:22 +0530

    User Route - Login functionality incl. token logic implementation done.

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const logger = require("../config/logger");
 const userRouter = express.Router();
+const jwt = require("jsonwebtoken");
+// Database Connection Handlers
+const notifDB = require("../config/mongo-db-connect");
+const vasooliDB = require("../config/mysql-connect");
 
 /**
  * @swagger
@@ -35,11 +39,43 @@ userRouter.post("/login", (req, res) => {
   const { body } = req;
   const { username, password } = body;
   logger.debug({ username, password });
-  res.json({
-    loginStatus: "SUCCESS",
-    token: "78984asd5adadada56sd46a54d6asd5sa4d6ad",
-    loginMsg: "Login Route for user",
-  });
+
+  //TODO: Check if user exists in DB
+  vasooliDB.query(
+    "SELECT * FROM users WHERE email = ?",
+    [username],
+    (err, results) => {
+      if (err || results.length === 0) {
+        res.status(400).send({
+          message: "User do not exist in our system",
+          success: false,
+          error: true,
+          ...err,
+        });
+      }
+
+      //TODO: Check if password is correct
+      const [user, ...rest] = results;
+      if (user.password !== password) {
+        res.status(401).send({
+          message: "Password you entered is Incorrect",
+          success: false,
+          error: true,
+        });
+      } else {
+        //TODO: Create JWT Token and Nullify the Password send it back
+        user.password = null;
+        const accessToken = jwt.sign(user, "secret");
+        res.status(200).send({
+          message: "Successfully Logged In",
+          success: true,
+          error: false,
+          token: accessToken,
+          ...user,
+        });
+      }
+    }
+  );
 });
 
 /**
@@ -128,12 +164,12 @@ userRouter.post("/logout", (req, res) => {
  *            type: "string"
  *            example: "user one"
  *          profilePic:
- *            format: "int64" 
+ *            format: "int64"
  *            example: 6
  *          city:
  *            type: "string"
  *            example: "sample city"
- *          
+ *
  *    responses:
  *      "200":
  *        description: "UpdateProfile Success"
