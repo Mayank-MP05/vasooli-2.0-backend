8ed8e81328107f7397d9eea4b17fa5a38c458729
Mayank-MP05 <mayank5pande@gmail.com>
2022-05-21 10:53:46 +0530

    Transaction Route - Create new transaction along with Swagger Docs implementation done.

--- routes/transactions.routes.js
+++ routes/transactions.routes.js
@@ -3,6 +3,10 @@ const logger = require("../config/logger");
 const requestLogger = require("../middleware/request-logger");
 const txnRouter = express.Router();
 
+// Database Connection Handlers
+const notifDB = require("../config/mongo-db-connect");
+const vasooliDB = require("../config/mysql-connect");
+
 /**
  * @swagger
  * /transactions/create:
@@ -14,27 +18,75 @@ const txnRouter = express.Router();
  *    operationId: "/transactions/create"
  *    produces:
  *    - "application/json"
- *    parameters: []
+ *    parameters:
+ *    - in: "body"
+ *      name: "body"
+ *      description: "Txn Create Payload"
+ *      required: true
+ *      schema:
+ *        type: "object"
+ *        properties:
+ *          userId:
+ *            type: "string"
+ *            example: 4
+ *          type:
+ *            type: "string"
+ *            example: "INC"
+ *          amount:
+ *            type: "int64"
+ *            example: 100
+ *          category:
+ *            type: "int64"
+ *            example: 0
+ *          date:
+ *            type: "date"
+ *            example: 	"2022-5-21 10:50:19"
+ *          description:
+ *            type: "string"
+ *            example: "Sample Txn Description"
  *    responses:
  *      "200":
  *        description: "Transaction added successfully"
- *        schema:
- *          type: "object"
- *          additionalProperties:
- *            type: "integer"
- *            format: "int32"
+ *    components:
+ *      securitySchemes:
+ *        bearerAuth:
+ *          type: http
+ *          scheme: bearer
+ *          bearerFormat: JWT
  *    security:
- *    - JWT: []
+ *      - bearerAuth: []
  *    securityDefinitions:
  *      JWT:
  *        type: apiKey
  *        in: header
  *        name: access_token
  */
-txnRouter.post("/create", (req, res) => {
+txnRouter.post("/create", requestLogger, (req, res) => {
   const { body } = req;
-  logger.debug(body);
-  res.send("Transaction Route for create");
+  const { userId, type, amount, category, date, description } = body;
+  //TODO: Push the new record txn data to DB
+  vasooliDB.query(
+    "INSERT INTO transactions (userId, type, amount, category, date, description) VALUES (?, ?, ?, ?, ?, ?)",
+    [userId, type, amount, category, date, description],
+    (err, results) => {
+      logger.info("%o %o", err, results);
+      if (err) {
+        res.status(500).send({
+          message: "Error in creating transaction",
+          success: false,
+          error: true,
+          ...err,
+        });
+      } else {
+        res.status(200).send({
+          message: "Transaction created successfully",
+          success: true,
+          error: false,
+          ...results,
+        });
+      }
+    }
+  );
 });
 
 txnRouter.get("/read", (req, res) => {
