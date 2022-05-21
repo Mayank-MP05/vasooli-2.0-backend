6739f9b575cf831702734b4f9e56bbeb224f0dcd
Mayank-MP05 <mayank5pande@gmail.com>
2022-05-21 14:37:06 +0530

    #10 Vasooli Route - Vasooli update by vasooliId along with Swagger Docs implementation done.

--- routes/vasooli.routes.js
+++ routes/vasooli.routes.js
@@ -136,8 +136,112 @@ vasooliRouter.get("/read/:userId", (req, res) => {
   );
 });
 
-vasooliRouter.put("/update/:id", (req, res) => {
-  res.send("vasooli Route for Update by id");
+/**
+ * @swagger
+ * /vasooli/update/{vasooliId}:
+ *  put:
+ *    tags:
+ *    - "Vasooli"
+ *    summary: "Update the particular vasooli record"
+ *    description: "Returns array of updated vasooli along with success and error flags"
+ *    operationId: "/vasooli/update/{vasooliId}"
+ *    produces:
+ *    - "application/json"
+ *    parameters:
+ *    - in: "path"
+ *      name: "vasooliId"
+ *      example: 2
+ *      description: "vasooli Id to Update"
+ *      required: true
+ *    - in: "body"
+ *      name: "body"
+ *      description: "vasooli Update Payload"
+ *      required: true
+ *      schema:
+ *        type: "object"
+ *        properties:
+ *          userId:
+ *            type: "int64"
+ *            example: 4
+ *          requestedTo:
+ *            type: "string"
+ *            example: "one@mail.com"
+ *          type:
+ *            type: "string"
+ *            example: "EXP"
+ *          amount:
+ *            type: "int64"
+ *            example: 100
+ *          category:
+ *            type: "int64"
+ *            example: 0
+ *          date:
+ *            type: "date"
+ *            example: 	"2022-5-21 10:50:19"
+ *          description:
+ *            type: "string"
+ *            example: "Updated Sample vasooli Description"
+ *          status:
+ *            type: "string"
+ *            example: "APPROVED"
+ *    responses:
+ *      "200":
+ *        description: "update vasooli by vasooliId success!!"
+ */
+vasooliRouter.put("/update/:vasooliId", (req, res) => {
+  const { body } = req;
+  const { vasooliId: vasooliIdX } = req.params;
+  const vasooliId = parseInt(vasooliIdX);
+
+  const { userId, requestedTo, amount, category, date, description, status } =
+    body;
+  vasooliDB.query(
+    "SELECT * from users where email = ?",
+    [requestedTo],
+    (errUser, resultUser) => {
+      if (errUser || resultUser.length === 0) {
+        res.status(400).json({
+          error: true,
+          success: false,
+          error: "Requested user not found",
+        });
+        return;
+      }
+      const requestedToUser = resultUser[0].userId;
+      //TODO: Update the vasooli record to DB
+      vasooliDB.query(
+        `UPDATE vasoolis
+        SET amount = ?, category = ?, date = ?, description = ?, requestedTo = ?, status = ? WHERE vasooliId = ?`,
+        [
+          amount,
+          category,
+          date,
+          description,
+          requestedToUser,
+          status,
+          vasooliId,
+        ],
+        (err, results) => {
+          logger.info("%o %o", err, results);
+          if (err) {
+            res.status(500).send({
+              message: "Error in updating vasooli record",
+              success: false,
+              error: true,
+              ...err,
+            });
+          } else {
+            res.status(200).send({
+              message: "Vasooli record updated successfully",
+              success: true,
+              error: false,
+              ...results,
+            });
+          }
+        }
+      );
+    }
+  );
 });
 
 vasooliRouter.delete("/delete/:id", (req, res) => {
